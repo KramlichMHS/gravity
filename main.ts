0141faef912327af8451084874e10012701cbcd6
Rob Kramlich <67026814+KramlichMHS@users.noreply.github.com>
2021-03-25 12:01:34 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,70 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (jumps < 2) {
+        sprite.vy = -100
+        jumps += 1
+    }
+})
+let jumps = 0
+let sprite: Sprite = null
+tiles.setTilemap(tilemap`level1`)
+sprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(sprite, 100, 0)
+sprite.setStayInScreen(true)
+jumps = 0
+forever(function () {
+    sprite.ay = 200
+    if (sprite.vx < 0) {
+        animation.runImageAnimation(
+        sprite,
+        assets.animation`rightWalk`,
+        200,
+        false
+        )
+    } else if (sprite.vx > 0) {
+        animation.runImageAnimation(
+        sprite,
+        assets.animation`leftWalk`,
+        200,
+        false
+        )
+    } else {
+        sprite.setImage(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `)
+    }
+    if (sprite.isHittingTile(CollisionDirection.Bottom)) {
+        jumps = 0
+    }
+})
